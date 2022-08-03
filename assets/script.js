0c7d7b41832fd9fea17e138735fc5f9810acb272
Vanese <vanesebuchanan@gmail.com>
2022-08-03 18:37:32 -0400

    Challenge5

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Moment.js for current date
+var today = moment().format("dddd, MMMM Do YYYY, h:mm");
+console.log(today)
+
+var currentDay = document.getElementById("currentDay")
+currentDay.textContent = today
+
+
+
+// allow text area to save entry when button is pushed
+
+localStorage.setItem(time, text);
+
+$("#8am .description").val(localStorage.getItem("hour8"));
+$("#9am .description").val(localStorage.getItem("hour9"));
+$("#10am .description").val(localStorage.getItem("hour10"));
+$("#11am .description").val(localStorage.getItem("hour11"));
+$("#12pm .description").val(localStorage.getItem("hour12"));
+$("#1pm .description").val(localStorage.getItem("hour1"));
+$("#2pm .description").val(localStorage.getItem("hour2"));
+$("#3pm .description").val(localStorage.getItem("hour3"));
+$("#4pm .description").val(localStorage.getItem("hour4"));
+$("#5pm .description").val(localStorage.getItem("hour5"));
+$("#6pm .description").val(localStorage.getItem("hour6"));
+$("#7pm .description").val(localStorage.getItem("hour7"));
+
+// text area for adding appointments and saving
+
+
